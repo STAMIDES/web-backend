6bf7b04276447c493e3ca1b813eb33f2a5cfa3c7
Rodri <ranitacafetera@gmail.com>
2024-07-03 19:16:16 -0300

    arreglo commit previo

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ log = logging.getLogger("routes")
 usuarios_router = APIRouter()
 
 @usuarios_router.get("/", dependencies=[Depends(JWTBearer())])
-def get_usuarios(skip: int = 0, limit: int = 10, , search: str = ''):
+def get_usuarios(skip: int = 0, limit: int = 10, search: str = ''):
     try:
         usuarios, cantidad = db.get_usuarios(skip=skip, limit=limit)
         return {"usuarios": usuarios, "cantidad": cantidad}
